102b44896666ed9eb4e4416794c7d56d0579a1ad
fer-caballero <fernando.caballero@globant.com>
2025-10-22 11:12:13 -0300

    login completo falta merge

--- tests/SwagLabsTest.spec.ts
+++ tests/SwagLabsTest.spec.ts
@@ -1,36 +1,24 @@
 import { test, expect } from '@playwright/test';
 import{ Login } from './Pages/Login';
 
-
-test('has title', async ({ page }) => {
-
+test('regression', async ({ page }) => {
   const loginPage = new Login(page);
-
+  await test.step('Dado que me encuentro en la página de Login de Swaglabs', async () => {
   await page.goto('https://www.saucedemo.com/v1/index.html');
-
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("Swag Labs");
-
-
-  await test.step('Rellenar usuario', async () => {
-    expect(loginPage.usernameTxt).toBeVisible();
-    await loginPage.fillUsername('standard_user');
-    expect(loginPage.usernameTxt).toHaveValue('standard_user');
-  })
+  await expect(page).toHaveTitle("Swag Labs");  
+  });
   
-  await test.step('Rellenar contraseña', async () => {
-    expect(loginPage.passwordTxt).toBeVisible();
-    await loginPage.fillPassword('secret_sauce');
-    expect(loginPage.passwordTxt).toHaveValue('secret_sauce');
-  })
-  
-  
-  await test.step('Click en boton login', async () => {
-    expect(loginPage.loginBtn).toBeVisible();
-    await loginPage.clickLogin();
-  })
+await test.step('Cuando ingreso usario: standard_user y contraseña: secret_sauce hago click en login', async () => {
+  expect(loginPage.usernameTxt).toBeVisible();
+  await loginPage.fillUsername('standard_user');
+  expect(loginPage.usernameTxt).toHaveValue('standard_user');
+  expect(loginPage.passwordTxt).toBeVisible();
+  await loginPage.fillPassword('secret_sauce');
+  expect(loginPage.passwordTxt).toHaveValue('secret_sauce');
+  expect(loginPage.loginBtn).toBeVisible();
+  await loginPage.clickLogin();
 });
 
-
-
-
+  await test.step('Entonces verifico Ingreso al Hompage de Swaglabs', async () => {});
+});
